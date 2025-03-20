0fdcb9fa956d4a613c6c5c68d71a86ddd1823962
Benjamin Axline <baxline@bu.edu>
2025-03-20 18:32:54 -0400

    initial

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # P2P-chat
+
+## TODO:
+- implement p2p chat, multiple clients
+- add in databases to store messages
+- 
---
+++ chat_server.py
@@ -0,0 +1,9 @@
+import socket
+import select
+import sys
+from _thread import *
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+if len(sys.argv) != :
